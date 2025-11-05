a6a269fd5713a8a31b5d9526adf6b9d77f084925
Sahil Saurav <153229295+Sahil-Saurav@users.noreply.github.com>
2025-11-05 23:58:01 +0530

    Stop tracking Httpdetails.kt

--- app/src/main/java/com/example/bookshelf/Common/Httpdetails.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookshelf.Common
-
-object Httpdetails {
-
-    val BASE_URL: String = "https://www.googleapis.com/books/v1/"
-    val API_KEY: String = "AIzaSyBcYi7Xa5Mgb-9dIF3eUlS-KzmHSx3EoEU"
-}
