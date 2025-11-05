7495d88950f6ef70c2c3ffb456a870fe8b2a3a4a
Sahil Saurav <153229295+Sahil-Saurav@users.noreply.github.com>
2025-11-06 00:03:47 +0530

    Added gitignore

---
+++ app/src/main/java/com/example/bookshelf/Common/Httpdetails.kt
@@ -0,0 +1,7 @@
+package com.example.bookshelf.Common
+
+object Httpdetails {
+
+    val BASE_URL: String = "https://www.googleapis.com/books/v1/"
+    val API_KEY: String = "AIzaSyBcYi7Xa5Mgb-9dIF3eUlS-KzmHSx3EoEU"
+}
